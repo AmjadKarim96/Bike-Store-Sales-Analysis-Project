d4a6d150256bc41e02e0a1c604f9351f77740de7
Amjad Karim <166842865+AmjadKarim96@users.noreply.github.com>
2024-04-25 20:13:56 +0200

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+SELECT*
+FROM EmployeeDemogrphics
+ORDER BY Age DESC, Gender DESC
+
+--SELECT DISTINCT Gender, COUNT(Gender) AS CountGender
+--FROM EmployeeDemogrphics
+--WHERE Age >31
+--GROUP by Gender
+--Order BY CountGender 
+
